d79c6901f085aeb556c64f67b2c471e629c63796
Pablo Gila-Herranz <pablo.gila.herranz@gmail.com>
2025-01-24 13:23:13 +0100

    more phys constants

--- aton/_version.py
+++ aton/_version.py
@@ -10,5 +10,5 @@ https://semver.org/
 
 """
 
-__version__ = 'v0.0.4a1'
+__version__ = 'v0.0.4a2'
 
--- aton/phys/units.py
+++ aton/phys/units.py
@@ -1,11 +1,9 @@
 """
 # Description
 
-This module contains useful constants and conversion factors.
-
-Constant values come from the [2022 CODATA](https://doi.org/10.48550/arXiv.2409.03787) Recommended Values of the Fundamental Physical Constants.
-
-Greek letters are [romanized](https://en.wikipedia.org/wiki/Romanization_of_Greek#Ancient_Greek).
+This module contains constants and conversion factors from the 
+[2022 CODATA](https://doi.org/10.48550/arXiv.2409.03787)
+Recommended Values of the Fundamental Physical Constants.
 
 
 # Index
@@ -21,10 +19,12 @@ Greek letters are [romanized](https://en.wikipedia.org/wiki/Romanization_of_Gree
     - [Universal](#universal)
     - [Electromagnetic](#electromagnetic)
     - [Atomic and nuclear](#atomic-and-nuclear)
-    - [Electron](#electron)
-    - [Proton](#proton)
-    - [Neutron](#neutron)
-    - [Deuteron](#deuteron)
+        - [General](#general)
+        - [Electron](#electron)
+        - [Proton](#proton)
+        - [Neutron](#neutron)
+        - [Deuteron](#deuteron)
+        - [Alpha particle](#alpha-particle)
     - [Physicochemical](#physicochemical)
 
 
@@ -37,9 +37,15 @@ length_in_angstroms = 10.0 * phys.bohr_to_A  # 5.29177210544
 phys.hbar   # 1.0545718176461565e-34
 ```
 
+In general, greek letters from constant names are
+[romanized](https://en.wikipedia.org/wiki/Romanization_of_Greek#Ancient_Greek),
+excepting $\\mu$ which uses `u`.
+
 ---
 
 ## Conversion factors
+
+For quick unit conversion, as `value_initial * initial_to_final`
 
 ### Energy
 Note that `cm1` refers to cm$^{-1}$.
@@ -97,7 +103,6 @@ kbar_to_GPa = 1.0 / GPa_to_kbar
 
 """---
 ### Time
-Note that `H` refers to hours.
 """
 H_to_s      = 3600.0
 s_to_H      = 1.0 / H_to_s
@@ -116,31 +121,52 @@ Using SI units unless stated otherwise.
 ### Universal
 """
 c = 299792458
-"""$c$ | Speed of light in vacuum, in m/s"""
-h = 6.62607015e-34      # J s
+"""$c$ | speed of light in vacuum, in m/s"""
+u0 = 1.25663706127e-6
+"""$\\mu_0$ | vacuum magnetic permeability, in N·A$^{-2}$ ($4\\pi\\alpha\\hbar/e^2 c$)"""
+e0 = 1.25663706127e-6
+"""$\\epsilon_0$ | vacuum electric permittivity, in F·m$^{-1}$ ($1/\\mu_0 c^2$)"""
+Z0 = 376.730313412
+"""$Z_0$ | characteristic impedance of vacuum, in $\\Omega$ ($\\mu_0 c$)"""
+G = 6.67430e-11
+"""Newtonian constant of gravitation, in m$^3$·kg$^{-1}$·s$^{-1}$"""
+h = 6.62607015e-34
 """$h$ | Planck constant, in J·s"""
 h_eV = h * J_to_eV
 """$h$ | Planck constant, in eV·s"""
 hbar = h / (2 * np.pi)
-"""$\\hbar$ | Reduced Planck constant, in J·s"""
+"""$\\hbar$ | reduced Planck constant, in J·s"""
 hbar_eV = h_eV / (2 * np.pi)
-"""$\\hbar$ | Reduced Planck constant, in eV·s
+"""$\\hbar$ | reduced Planck constant, in eV·s
 
 ---
 ### Electromagnetic
 """
 e = 1.602176634e-19
-"""$e$ | Elementary charge, in C"""
-muB = 9.2740100657e-24
+"""$e$ | elementary charge, in C"""
+P0 = 2.067833848e-15
+"""$\\Phi_0$ | magnetic flux quantum, in Wb ($2\\pi\\hbar/(2e)$)"""
+G0 = 7.748091729e-5
+"""$G_0$ | conductance quantum, in S ($2e^2/2\\pi h$)"""
+KJ = 483597.8484e9
+"""$K_J$ | Josephson constant, in Hz·V$^{-1}$ (2e/h)"""
+RK = 25812.80745
+"""$R_K$ | von Klitzing constant, in $\\Omega$ ($\\mu_0 c/2\\alpha = 2\\pi\\hbar/e^2$)"""
+uB = 9.2740100657e-24
 """$\\mu_B$ | Bohr magneton, in J·T$^{-1}$ ($e\\hbar / 2m_e$)""" 
-muN = 5.0507837393e-27
-"""$\\mu_N$ | Nuclear magneton, in J·T$^{-1}$ ($e\\hbar / 2m_p$)
+uN = 5.0507837393e-27
+"""$\\mu_N$ | nuclear magneton, in J·T$^{-1}$ ($e\\hbar / 2m_p$)
 
 ---
 ### Atomic and nuclear
+#### General
 """
 a = 7.2973525643e-3
-"""$\\alpha$ | Fine-structure constant ($e^2 / 4 \\pi \\epsilon_0 \\hbar c$)"""
+"""$\\alpha$ | fine-structure constant ($e^2 / 4 \\pi \\epsilon_0 \\hbar c$)"""
+a1 = 137.035999177
+"""$\\alpha^{-1}$ | inverse fine-structure constant"""
+cRinf = 3.2898419602500e15 
+"""$cR\\infty$ | Rydberg frequency, in Hz ($\\alpha^2m_e c^2/2h = E_h/2h$)"""
 Rinf = 10973731.568157
 """$R\\infty$ | Rydberg constant, in $[m^{-1}]^a$"""
 a0 = 5.29177210544e-11
@@ -149,74 +175,88 @@ Eh = 4.3597447222060e-18
 """$E_h$ | Hartree energy, in J ($\\alpha^2m_ec^2=e^2/4\\pi\\epsilon_0a_0=2h c R_{\\infty}$)
 
 ---
-### Electron
+#### Electron
 """
 me = 9.1093837139-31
-"""$m_e$ | Electron mass, in kg"""
+"""$m_e$ | electron mass, in kg"""
 me_uma = 5.485799090441e-4
-"""$m_e$ | Electron mass, in uma"""
+"""$m_e$ | electron mass, in uma"""
 mec2 = 8.1871057880e-14
-"""$m_e c^2$ | Electron mass energy equivalent, in J"""
+"""$m_e c^2$ | electron mass energy equivalent, in J"""
 mec2_eV = 510998.95069
-"""$m_e c^2$ | Electron mass energy equivalent, in eV"""
+"""$m_e c^2$ | electron mass energy equivalent, in eV"""
 lC = 2.42631023538e-12
 """$\\lambda_C$ | Compton wavelength, in $[m]^a$"""
 re = 2.8179403205e-15
-"""$r_e$ | Classical electron radius, in m ($\\alpha^2 a_0$)"""
+"""$r_e$ | classical electron radius, in m ($\\alpha^2 a_0$)"""
 se = 6.6524587051e-29
 """$\\sigma_e$ | Thomson cross section, in m$^2$ ($(8\\pi / 3)r_e^2$)"""
-mue = -9.2847646917e-24
-"""$\\mu_e$ | Electron magnetic moment, in J·T$^{-1}$
+ue = -9.2847646917e-24
+"""$\\mu_e$ | electron magnetic moment, in J·T$^{-1}$
 
 ---
-### Proton
+#### Proton
 """
 mp = 1.67262192595-27
-"""$m_p$ | Proton mass, in kg"""
+"""$m_p$ | proton mass, in kg"""
 mp_uma = 1.0072764665789
-"""$m_p$ | Proton mass, in uma"""
+"""$m_p$ | proton mass, in uma"""
 mpc2 = 1.50327761802e-10
-"""$m_p c^2$ | Proton mass energy equivalent, in J"""
+"""$m_p c^2$ | proton mass energy equivalent, in J"""
 mpc2_eV = 938272089.43
-"""$m_p c^2$ | Proton mass energy equivalent, in eV"""
+"""$m_p c^2$ | proton mass energy equivalent, in eV"""
+mpme = 1836.152673426
+"""$m_p/m_e$ | proton-electron mass ratio"""
 lCp = 1.32140985360e-15
-"""$\\lambda_{C,p}$ | Proton Compton wavelength, in $[m]^a$"""
+"""$\\lambda_{C,p}$ | proton Compton wavelength, in $[m]^a$"""
 rp = 8.4075e-16
-"""$r_p$ | Proton rms charge radius, in m"""
-mup = 1.41060679545e-26
-"""$\\mu_p$ | Proton magnetic moment, in J·T$^{-1}$
+"""$r_p$ | proton rms charge radius, in m"""
+up = 1.41060679545e-26
+"""$\\mu_p$ | proton magnetic moment, in J·T$^{-1}$
 
 ---
-### Neutron
+#### Neutron
 """
 mn = 1.67492750056e-27
-"""$m_n$ | Neutron mass, in kg"""
+"""$m_n$ | neutron mass, in kg"""
 mn_uma = 1.00866491606
-"""$m_n$ | Neutron mass, in uma"""
+"""$m_n$ | neutron mass, in uma"""
 mnc2 = 1.50534976514e-10
-"""$m_n c^2$ | Neutron mass energy equivalent, in J"""
+"""$m_n c^2$ | neutron mass energy equivalent, in J"""
 mnc2_eV = 939565421.94
-"""$m_n c^2$ | Neutron mass energy equivalent, in eV"""
+"""$m_n c^2$ | neutron mass energy equivalent, in eV"""
 lCn = 1.31959090382e-15
-"""$\\lambda_{C,n}$ | Neutron compton wavelength, in $[m]^a$"""
-mun = -9.6623653e-27
-"""$\\mu_n$ | Neutron magnetic moment, in J·T$^{-1}$
+"""$\\lambda_{C,n}$ | neutron compton wavelength, in $[m]^a$"""
+un = -9.6623653e-27
+"""$\\mu_n$ | neutron magnetic moment, in J·T$^{-1}$
 
 ---
-### Deuteron
+#### Deuteron
 """
 md = 3.3435837768e-27
-"""$m_d$ | Deuteron mass, in kg"""
+"""$m_d$ | deuteron mass, in kg"""
 md_uma = 2.013553212544
-"""$m_d$ | Deuteron mass, in uma"""
+"""$m_d$ | deuteron mass, in uma"""
 mdc2 = 3.00506323491e-10
-"""$m_d c^2$ | Deuteron mass energy equivalent, in J"""
+"""$m_d c^2$ | deuteron mass energy equivalent, in J"""
 mdc2_eV = 1875612945
-"""$m_d c^2$ | Deuteron mass energy equivalent, in eV"""
+"""$m_d c^2$ | deuteron mass energy equivalent, in eV"""
 rd = 2.12778e-15
-"""$r_d$ | Deuteron rms charge radius, in m"""
-mud = 4.330735087e-27
-"""$\\mu_d$ | Deuteron magnetic moment, in J·T$^{-1}$
+"""$r_d$ | deuteron rms charge radius, in m"""
+ud = 4.330735087e-27
+"""$\\mu_d$ | deuteron magnetic moment, in J·T$^{-1}$
+
+---
+#### Alpha particle
+"""
+ma = 6.6446573450e-27
+"""$m_\\alpha$ | alpha particle mass, in kg"""
+mac2 = 5.9719201997e-10
+"""$m_\\alpha$ | alpha particle mass energy equivalent, in J"""
+mac2_eV = 3727379411.8
+"""$m_\\alpha$ | alpha particle mass energy equivalent, in eV"""
+ra = 1.6785e-15
+"""$r_\\alpha$ | alpha particle rms charge radius, in m
 
 ---
 ### Physicochemical
@@ -228,13 +268,15 @@ k = 1.380649e-23
 k_eV = 8.617333262e-5
 """$k$ | Boltzmann constant, in eV·K$^{-1}$"""
 mu = 1.66053906892e-27
-"""$m_u$ | Atomic mass constant, in kg ($\\frac{1}{12}m(^{12}C)$)"""
+"""$m_u$ | atomic mass constant, in kg ($\\frac{1}{12}m(^{12}C)$)"""
 muc2 = 1.49241808768e-10
-"""$m_u c^2$ | Atomic mass constant energy equivalent, in J"""
+"""$m_u c^2$ | atomic mass constant energy equivalent, in J"""
 muc2_eV = 931494103.72
-"""$m_u c^2$ | Atomic mass constant energy equivalent, in eV"""
+"""$m_u c^2$ | atomic mass constant energy equivalent, in eV"""
 R = 8.314462618
-"""$R$ | Molar gas constant, in J·mol$^{-1}$K$^{-1}$ ($N_A k$)"""
+"""$R$ | molar gas constant, in J·mol$^{-1}$K$^{-1}$ ($N_A k$)"""
 F = 96485.33212
 """$F$ | Faraday constant, in C·mol$^{-1}$ ($N_A e$)"""
+s = 5.670374419e-8
+"""$\\sigma$ | Stefan-Boltzmann constant, in W·m$^{-2}$·K$^{-4}$ ($(\\pi^2/60)k^4 /\\hbar^3 c^2$)"""
 
