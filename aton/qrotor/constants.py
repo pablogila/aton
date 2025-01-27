fixed QRotor units

aton/qrotor/constants.py
@@ -37,39 +37,36 @@ r_NH = distance_NH * np.sin(np.deg2rad(angle_NH)) * phys.A_to_m
 """Rotation radius of the amine group, in meters."""
 
 # Inertia, SI units
-I_CH = 3 * (phys.atoms['H'].mass * phys.amu_to_kg * r_CH**2)
+I_CH3 = 3 * (phys.atoms['H'].mass * phys.amu_to_kg * r_CH**2)
 """Inertia of CH3, in kg·m^2."""
-I_CD = 3 * (phys.atoms['H'].isotope[2].mass * phys.amu_to_kg * r_CH**2)
+I_CD3 = 3 * (phys.atoms['H'].isotope[2].mass * phys.amu_to_kg * r_CH**2)
 """Inertia of CD3, in kg·m^2."""
-I_NH = 3 * (phys.atoms['H'].mass * phys.amu_to_kg * r_NH**2)
+I_NH3 = 3 * (phys.atoms['H'].mass * phys.amu_to_kg * r_NH**2)
 """Inertia of NH3, in kg·m^2."""
-I_ND = 3 * (phys.atoms['H'].isotope[2].mass * phys.amu_to_kg * r_NH**2)
+I_ND3 = 3 * (phys.atoms['H'].isotope[2].mass * phys.amu_to_kg * r_NH**2)
 """Inertia of ND3, in kg·m^2."""
 
 # Rotational energy.
-B_CH = ((phys.hbar_eV**2) / (2 * I_CH))
+B_CH3 = ((phys.hbar**2) / (2 * I_CH3)) * phys.J_to_meV
 """Rotational energy of CH3, in eV·s/kg·m^2."""
-B_CD = ((phys.hbar_eV**2) / (2 * I_CD))
+B_CD3 = ((phys.hbar**2) / (2 * I_CD3)) * phys.J_to_meV
 """Rotational energy of CD3, in eV·s/kg·m^2."""
-B_NH = ((phys.hbar_eV**2) / (2 * I_NH))
+B_NH3 = ((phys.hbar**2) / (2 * I_NH3)) * phys.J_to_meV
 """Rotational energy of NH3, in eV·s/kg·m^2."""
-B_ND = ((phys.hbar_eV**2) / (2 * I_ND))
-"""Rotational energy of ND3, in eV·s/kg·m^2."""
+B_ND3 = ((phys.hbar**2) / (2 * I_ND3)) * phys.J_to_meV
+"""Rotational energy of ND3, in meV·s/kg·m^2."""
 
 # Potential constants from titov2023 [C1, C2, C3, C4, C5]
 constants_titov2023 = [
-    [2.7860, 0.0130,-1.5284,-0.0037,-1.2791],
-    [2.6507, 0.0158,-1.4111,-0.0007,-1.2547],
-    [2.1852, 0.0164,-1.0017, 0.0003,-1.2061],
-    [5.9109, 0.0258,-7.0152,-0.0168, 1.0213],
-    [1.4526, 0.0134,-0.3196, 0.0005,-1.1461]
+    [2.7860, 0.0130,-1.5284,-0.0037,-1.2791],  # ZIF-8
+    [2.6507, 0.0158,-1.4111,-0.0007,-1.2547],  # ZIF-8 + Ar-1
+    [2.1852, 0.0164,-1.0017, 0.0003,-1.2061],  # ZIF-8 + Ar-{1,2}
+    [5.9109, 0.0258,-7.0152,-0.0168, 1.0213],  # ZIF-8 + Ar-{1,2,3}
+    [1.4526, 0.0134,-0.3196, 0.0005,-1.1461],  # ZIF-8 + Ar-{1,2,4}
     ]
 """Potential constants from
 [K. Titov et al., Phys. Rev. Mater. 7, 073402 (2023)](https://link.aps.org/doi/10.1103/PhysRevMaterials.7.073402)
 for the `aton.qrotor.potential.titov2023` potential.
+In meV units.
 """
-constants_titov2023_zero = [
-    [0,0,0,0,0]
-    ]
-"""Zero potential constants for the `aton.qrotor.potential.titov2023` potential."""
 

aton/qrotor/plot.py
@@ -54,7 +54,7 @@ def potential(data):
     for system in dataset.systems:
         plt.plot(system.grid, system.potential_values, marker='', linestyle='-')
     plt.xlabel('Angle / rad')
-    plt.ylabel('Potential energy / eV')
+    plt.ylabel('Potential energy / meV')
     plt.xticks([0, np.pi/2, np.pi, 3*np.pi/2, 2*np.pi], ['0', r'$\frac{\pi}{2}$', r'$\pi$', r'$\frac{3\pi}{2}$', r'$2\pi$'])
     plt.title(dataset.comment)
     plt.show()
@@ -85,15 +85,8 @@ def energy_DEV(data:QExp):
 
     V_linestyle = '-'
 
-    units = data.units
-    E_units = 'meV'
-    if 'meV' in units or 'mev' in units:
-        E_units = 'meV'
-    elif 'eV' in units or 'ev' in units:
-        E_units = 'eV'
-
     title = data.comment
-    ylabel_text = f'Energy / {E_units}'
+    ylabel_text = f'Energy / eV'
     xlabel_text = 'Angle / radians'
 
     plt.figure(figsize=(10, 6))

aton/qrotor/potential.py
@@ -30,19 +30,20 @@ import aton.st.alias as alias
 import aton.st.file as file
 import aton.interface.qe as qe
 import aton.phys as phys
+from aton._version import __version__
 
 
 def load(
         filepath:str='potential.dat',
         system:QSys=None,
         angle_unit:str='deg',
-        energy_unit:str='eV',
+        energy_unit:str='meV',
         ) -> QSys:
     """Read a potential rotational energy dataset.
 
     The file in `filepath` should contain two columns with angle and potential energy values.
-    Degrees and eV are assumed as default units unless stated in `angle_unit` and `energy_unit`.
-    Units will be converted automatically to radians and eV.
+    Degrees and meV are assumed as default units unless stated in `angle_unit` and `energy_unit`.
+    Units will be converted automatically to radians and meV.
     """
     file_path = file.get(filepath)
     system = QSys() if system is None else system
@@ -64,12 +65,12 @@ def load(
     else:
         raise ValueError(f"Angle unit '{angle_unit}' not recognized.")
     # Save energies to numpy arrays
-    if energy_unit.lower() in alias.units['meV']:
-        potentials = np.array(potentials) * 1000
-    elif energy_unit.lower() in alias.units['eV']:
+    if energy_unit.lower() in alias.units['eV']:
+        potentials = np.array(potentials) * phys.eV_to_meV
+    elif energy_unit.lower() in alias.units['meV']:
         potentials = np.array(potentials)
     elif energy_unit.lower() in alias.units['Ry']:
-        potentials = np.array(potentials) * phys.Ry_to_eV
+        potentials = np.array(potentials) * phys.Ry_to_meV
     else:
         raise ValueError(f"Energy unit '{energy_unit}' not recognized.")
     # Set the system
@@ -84,6 +85,7 @@ def from_qe(
         output:str='potential.dat',
         include:list=['.out'],
         ignore:list=['slurm-'],
+        energy_unit:str='meV',
         ) -> None:
     """Creates a potential data file from Quantum ESPRESSO outputs.
 
@@ -94,10 +96,23 @@ def from_qe(
     Files can be filtered by those containing the specified `filters`,
     excluding those containing any string from the `ignore` list. 
     The `output` name is `potential.dat` by default.
+
+    Energy values are saved to meV by dafault, unless specified in `energy_unit`.
     """
     folder = file.get_dir(folder)
     files = file.get_list(folder=folder, include=include, ignore=ignore, abspath=True)
-    potential_data = '# Angle/deg    Potential/eV\n'
+    # Set header
+    potential_data = f'# Potential values imported with ATON {__version__}\n'
+    potential_data += '# https://pablogila.github.io/ATON\n'
+    potential_data += '# ------------------------------------------\n'
+    if energy_unit.lower() in alias.units['eV']:
+        potential_data += '# Angle/deg    Potential/eV\n'
+    elif energy_unit.lower() in alias.units['meV']:
+        potential_data += '# Angle/deg    Potential/meV\n'
+    elif energy_unit.lower() in alias.units['Ry']:
+        potential_data += '# Angle/deg    Potential/Ry\n'
+    else:
+        potential_data += '# Angle/deg    Potential/meV\n'
     potential_data_list = []
     print('Extracting the potential as a function of the angle...')
     print('----------------------------------')
@@ -113,7 +128,15 @@ def from_qe(
             print(f'x   {filename}')
             counter_errors += 1
             continue
-        energy = content['Energy'] * phys.Ry_to_eV
+        if energy_unit.lower() in alias.units['eV']:
+            energy = content['Energy'] * phys.Ry_to_eV
+        elif energy_unit.lower() in alias.units['meV']:
+            energy = content['Energy'] * phys.Ry_to_meV
+        elif energy_unit.lower() in alias.units['Ry']:
+            energy = content['Energy']
+        else:
+            print(f"WARNING: Energy unit '{energy_unit}' not recognized, using meV instead.")
+            energy = content['Energy'] * phys.Ry_to_meV
         splits = filename.split('_')
         angle = splits[-1].replace('.out', '')
         angle = float(angle)
@@ -137,6 +160,7 @@ def from_qe(
 
 def interpolate(system:QSys) -> QSys:
     """Interpolates the current `aton.qrotor.classes.QSys.potential_values` to a new grid of size `aton.qrotor.classes.QSys.gridsize`."""
+    print(f"Interpolating potential to a grid of size {system.gridsize}...")
     V = system.potential_values
     grid = system.grid
     gridsize = system.gridsize
@@ -161,6 +185,10 @@ def solve(system:QSys):
 
     If a bigger `QSys.gridsize` is provided,
     the potential is also interpolated to the new gridsize.
+
+    This function provides basic solving of the potential energy function.
+    To interpolate to a new grid and correct the potential offset after solving,
+    check `aton.qrotor.solve.potential()`.
     """
     data = deepcopy(system)
     # Is there a potential_name?